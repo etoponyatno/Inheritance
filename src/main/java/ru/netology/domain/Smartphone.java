d1b76adebe0867f3097dce2c59dbb3eec0b6a7c2
Petr Filatov <pmfilatov@gmail.com>
2020-10-25 12:41:42 +0300

    Reformat code

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -4,45 +4,45 @@ import java.util.Objects;
 
 
 public class Smartphone extends Product {
-  private String producer;
+    private String producer;
 
-  public Smartphone() {
-    super();
-  }
+    public Smartphone() {
+        super();
+    }
 
-  public Smartphone(int id, String name, int price, String producer) {
-    super(id, name, price);
-    this.producer = producer;
-  }
+    public Smartphone(int id, String name, int price, String producer) {
+        super(id, name, price);
+        this.producer = producer;
+    }
 
-  public String getProducer() {
-    return producer;
-  }
+    public String getProducer() {
+        return producer;
+    }
 
-  public void setProducer(String producer) {
-    this.producer = producer;
-  }
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
-    Smartphone that = (Smartphone) o;
-    return producer.equals(that.producer);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Smartphone that = (Smartphone) o;
+        return producer.equals(that.producer);
+    }
 
-  @Override
-  public String toString() {
-    return "Smartphone{" +
-            "producer='" + producer + '\'' +
-            '}';
-  }
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "producer='" + producer + '\'' +
+                '}';
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), producer);
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), producer);
 
 
-  }
+    }
 }
